89da4108e46cc80e3abcb0722ee745a0719cc75b
Ștefan Chichișan <74013165+stefandreich@users.noreply.github.com>
2024-10-24 11:53:44 +0300

    conditional file download

---
+++ romeo.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [[ -f 'romeo.txt' ]]; then
+    echo 'File already exists'
+else
+    echo 'Downloading file...'
+    curl https://downloads.codingcoursestv.eu/055%20-%20bash/if/romeo.txt -o romeo.txt --silent
+    if [[ $? -eq 0 ]]; then
+        echo 'Download completed successfully'
+    else
+        echo 'Download failed' >&2
+        exit 1
+    fi
+fi
+
+word_count="$(grep -c 'love' romeo.txt)"
+echo "The word 'love' is found in ${word_count} lines."
